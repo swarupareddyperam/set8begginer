0ffc2cfb300ae9ffb7ae21c2088424ed45a870ec
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-13 20:31:44 +0530

    Create 74.java

---
+++ 74.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		float f;
+		Scanner sc=new Scanner(System.in);
+		f=sc.nextFloat();
+		int d=(int)Math.ceil(f);
+		System.out.println(d);
+		
+		
+	}
+}
