273c6c65173d52b18ddc0191cb5cfc7277d3cd8f
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-14 07:28:57 +0530

    Create 80.java

---
+++ 80.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n,i,rem=0;
+		n=sc.nextInt();
+		while(n!=0)
+		{
+			rem=n%10;
+			if(rem%2!=0)
+			{
+				System.out.print(rem+" ");
+			}
+			n=n/10;
+		}
+	}
+}
